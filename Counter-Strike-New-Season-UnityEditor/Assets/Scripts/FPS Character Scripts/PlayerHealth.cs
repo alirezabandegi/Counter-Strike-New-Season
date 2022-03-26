90f5f7b8996f4c74fa0d93846847b6ba33d54d9b
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2022-03-26 16:12:46 +0200

    Add porject Unity Editor

---
+++ Counter-Strike-New-Season-UnityEditor/Assets/Scripts/FPS Character Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour
+{
+
+    [SyncVar]
+    public float health = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        health -= damage;
+
+        print("helloooooooooooo");
+
+        if (health <= 0f)
+        {
+            
+        }
+
+    }
+}
---
+++ Counter-Strike-New-Season-UnityEditor/Assets/Scripts/FX Scripts/DestroyAfterTime.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+
+    public float timer = 1f;
+
+    void Start()
+    {
+        Destroy (gameObject, timer);
+    }
+
+    
+}
